c7a2317c6fe1052a87098100c85ab07dece72d50
AdarshaKumarMaharana <eadarsha2003@gmail.com>
2024-09-04 08:21:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// !    navbar menu js
+let navbar = document.querySelectorAll(".nav-link");
+let menuClose = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click",() => {
+        menuClose.classList.remove("show");
+    })
+})
+
+
+
+// !     countDown timer js
+var day = document.querySelector("#days");
+var hour = document.querySelector("#hours");
+var min = document.querySelector("#min");
+var sec = document.querySelector("#sec");
+
+    var nextdate = new Date("July 20, 2024 00:00:00").getTime();
+    
+    var countdown = setInterval(() => {
+        var currentdate = new Date().getTime();
+        var mydate = nextdate - currentdate;
+
+        var days = Math.floor(mydate / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((mydate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mins = Math.floor((mydate % (1000 * 60 * 60)) / (1000 * 60));
+        var secs = Math.floor((mydate % (1000 * 60)) / 1000)
+
+        day.innerHTML = days;
+        hour.innerHTML = hours;
+        min.innerHTML = mins;
+        sec.innerHTML = secs;
+
+    }, 1000);
+
+
+
+// !   SCROLL TO TOP JS
+function scrollTop() {
+    let scrollTopBtn = document.querySelector("#scrollUp");
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 600){
+            scrollTopBtn.classList.add("active")
+        }
+        else{
+            scrollTopBtn.classList.remove("active")
+        }
+    }
+}
+scrollTop();
